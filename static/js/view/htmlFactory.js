d1028e25e44fa0c9f2a529df2985c25a83b87f7f
Eagle-Thunder <eaglebr1@gmail.com>
2022-02-07 10:31:24 +0200

    prepped for acordeon

--- static/js/view/htmlFactory.js
+++ static/js/view/htmlFactory.js
@@ -21,10 +21,25 @@ export function htmlFactory(template) {
 }
 
 function boardBuilder(board) {
-    return `<div class="board-container">
-                <div class="board" data-board-id=${board.id}>${board.title}</div>
-                <button class="toggle-board-button" data-board-id="${board.id}">Show Cards</button>
-            </div>`;
+    // return `<div class="board-container">
+    //             <div class="board" data-board-id=${board.id}>${board.title}</div>
+    //             <button class="toggle-board-button" data-board-id="${board.id}">Show Cards</button>
+    //         </div>`;
+
+    return `
+    <section class="accordion" id="section-${board.id}" data-board-id="${board.id}">
+    <div class="accordion-item">
+    <h2 class="accordion-header" id="board-heading-${board.id}">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#board-collapse-${board.id}" aria-expanded="true" aria-controls="board-collapse-${board.id}">
+      </button>
+    </h2>
+    <div id="board-collapse-${board.id}" class="accordion-collapse collapse show" aria-labelledby="board-heading-${board.id}" data-bs-parent="#section-${board.id}">
+      <div id="board-body-${board.id}" class="accordion-body">
+      </div>
+    </div>
+    </div>
+  </section>
+`
 }
 
 function cardBuilder(card) {
